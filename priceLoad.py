e828b3a567166009c8a893ce35d0e3c46ac52121
Olav Garborg <olav.garborg@nov.com>
2024-12-27 10:32:10 +0100

    11

--- priceLoad.py
+++ priceLoad.py
@@ -401,6 +401,7 @@ def set_charging_amperage_(amperage):
             "Authorization": f"Bearer {access_token}",
             "Content-Type": "application/json"
         }
+        amperage = 11
         payload = {
             "maxChargeCurrent": amperage,
             "minChargeCurrent": amperage  # Assuming min and max are set the same for control
